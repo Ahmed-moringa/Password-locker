13684c86e869503e5505668abfd87e43f04ffd63
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2021-12-07 01:15:34 +0300

    run test to check user instance saved

--- locker_test.py
+++ locker_test.py
@@ -8,15 +8,22 @@ class TestClass(unittest.TestCase):
     """
     def setUp(self):
         """
-        Method that runs before each individual test methods run.
+        method that runs before each individual test methods run.
         """
         self.new_user = User('Ahmed','qwerty123')
 
     def test_init(self):
         """
-        test case to chek if the object has been initialized correctly
+        test case to check if the object has been initialized correctly
         """
         self.assertEqual(self.new_user.username,'Ahmed')
         self.assertEqual(self.new_user.password,'qwerty123')
+
+    def test_save_user(self):
+        """
+        test case to test if a new user instance has been saved into the User list
+        """
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
 if __name__ == "__main__":
     unittest.main()
